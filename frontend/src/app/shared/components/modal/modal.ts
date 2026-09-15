6e693ce65d4e4e0718a6e128d7475ef6601ba133
Gerson <villarroelpoma2@gmail.com>
2026-09-15 09:03:23 -0400

    modal generico

--- frontend/src/app/shared/components/modal/modal.ts
+++ frontend/src/app/shared/components/modal/modal.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
@@ -6,4 +6,11 @@ import { Component } from '@angular/core';
   templateUrl: './modal.html',
   styleUrl: './modal.scss',
 })
-export class Modal {}
+export class Modal {
+  readonly abierto = input(false);
+  readonly cerrado = output<void>();
+
+  cerrar(): void {
+    this.cerrado.emit();
+  }
+}
