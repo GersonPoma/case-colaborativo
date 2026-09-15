c218a449e6a007eaa0800b36543b822eca69ebff
Gerson <villarroelpoma2@gmail.com>
2026-09-15 17:13:14 -0400

    simplificar la barra superior tras mover las herramientas de dibujo al toolbox

--- frontend/src/app/modules/canvas/components/toolbar/toolbar.ts
+++ frontend/src/app/modules/canvas/components/toolbar/toolbar.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-toolbar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './toolbar.html',
   styleUrl: './toolbar.scss',
 })
-export class Toolbar {}
+export class Toolbar {
+  readonly conectado = input(false);
+  readonly puedeEditar = input(false);
+  readonly proyectoId = input.required<number>();
+  readonly guardandoVersion = input(false);
+  readonly puedeDeshacer = input(false);
+
+  readonly guardarVersion = output<void>();
+  readonly centrar = output<void>();
+  readonly deshacer = output<void>();
+}
